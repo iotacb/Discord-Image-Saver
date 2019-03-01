b4604ce4e70cc190a205fe2c6ed46f5bfcbd3ae0
Christopher <40664634+iotacb@users.noreply.github.com>
2019-03-01 17:05:35 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,11 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace DiscordImageSaver
 {
     public partial class Form1 : Form
     {
+
+        [DllImport(@"urlmon.dll", CharSet = CharSet.Auto)]
+        private extern static System.UInt32 FindMimeFromData(
+            System.UInt32 pBC,
+            [MarshalAs(UnmanagedType.LPStr)] System.String pwzUrl,
+            [MarshalAs(UnmanagedType.LPArray)] byte[] pBuffer,
+            System.UInt32 cbSize,
+            [MarshalAs(UnmanagedType.LPStr)] System.String pwzMimeProposed,
+            System.UInt32 dwMimeFlags,
+            out System.UInt32 ppwzMimeOut,
+            System.UInt32 dwReserverd
+        );
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +35,8 @@ namespace DiscordImageSaver
 
         String discordCachePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Discord/Cache/";
         String savePath = string.Empty;
+
+        public Dictionary<string, string> ImageTypes { get; private set; }
 
         private void bunifuFlatButton1_Click(object sender, EventArgs e)
         {
@@ -40,13 +57,41 @@ namespace DiscordImageSaver
                 foreach (String file in files)
                 {
                     String fileName = file.Substring(discordCachePath.Length);
-                    if (bunifuCheckbox1.Checked)
+
+                    if (fileName.StartsWith("data") || fileName.StartsWith("index"))
                     {
-                        Directory.Move(file, Path.Combine(savePath, fileName + ".png"));
-                    } else
-                    {
-                        File.Copy(Path.Combine(discordCachePath, fileName), Path.Combine(savePath, fileName + ".png"), true);
+                        continue;
                     }
+
+                    byte[] buffer = new byte[256];
+                    using (FileStream fileStream = new FileStream(discordCachePath + fileName, FileMode.Open, FileAccess.Read))
+                    {
+                        if (fileStream.Length >= 256)
+                        {
+                            fileStream.Read(buffer, 0, 256);
+                        } else
+                        {
+                            fileStream.Read(buffer, 0, buffer.Length);
+                        }
+                    } try
+                    {
+                        System.UInt32 mimeType;
+                        FindMimeFromData(0, null, buffer, 256, null, 0, out mimeType, 0);
+                        System.IntPtr mimeTypePtr = new IntPtr(mimeType);
+                        string mime = Marshal.PtrToStringUni(mimeTypePtr);
+
+                        String type = "." + splitText(mime);
+
+                        if (bunifuCheckbox1.Checked)
+                        {
+                            Directory.Move(file, Path.Combine(savePath, fileName + Path.GetRandomFileName() + type));
+                        } else
+                        {
+                            File.Copy(Path.Combine(discordCachePath, fileName), Path.Combine(savePath, fileName + Path.GetRandomFileName() + type), true);
+                        }
+                    }
+                    catch (Exception) { }
+
                 }
                 bunifuFlatButton3.Enabled = true;
             } else
@@ -64,6 +109,16 @@ namespace DiscordImageSaver
                 savePath = dialog.SelectedPath;
                 bunifuMetroTextbox1.Text = savePath;
             }
+        }
+
+        private String splitText(String mimeType)
+        {
+            String mime = mimeType.Split('/')[1];
+            if (mime.StartsWith("x-"))
+            {
+                mime = mime.Substring(2);
+            }
+            return mime;
         }
     }
 }
